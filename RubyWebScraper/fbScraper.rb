e75499036f6fd033c73fa77bf2968d2647cb3474
Sachin <sachinbhat.as@gmail.com>
2014-09-21 12:50:53 -0700

    committing all files together

--- RubyWebScraper/fbScraper.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'FbConsole.rb'
-
-fb = FbConsole.new
-fb.login("thunderbooolt@gmail.com","sairam#99")
-fb.notifications()
-fb.Home
-fb.feed_items(most_recent = true)
-fb.update_status("I am a hunter")
